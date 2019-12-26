19a49d3bc14b53684c8affbb55df9b1f665a017a
Rohini Kavitake <58133511+rohinikavitake@users.noreply.github.com>
2019-12-26 13:24:32 +0530

    Delete ass4.py

--- ass4.py
+++
@@ -1,8 +0,0 @@
-friends=["sneha","rohini","shivani","vishal"]
-print(friends[1])
-print(friends[1:])
-print(friends[1:3])
-print(friends[::-1])
-print(friends[:1])
-friends[1]="sweety"
-print(friends)
